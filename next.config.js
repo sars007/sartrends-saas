c61b66299a92a92c6a107c3e17903b4e64e8760d
Auto User <auto@example.com>
2026-04-06 01:49:56 +0500

    FINAL FIX: disable api build execution

--- next.config.js
+++ next.config.js
@@ -5,5 +5,9 @@ const nextConfig = {
   typescript: {
     ignoreBuildErrors: true,
   },
+  experimental: {
+    serverComponentsExternalPackages: ['@prisma/client'],
+  },
 }
+
 module.exports = nextConfig
